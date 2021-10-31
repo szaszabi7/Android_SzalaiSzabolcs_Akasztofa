307b614bcc118672f38b9084d6a7a62fbdd7006b
Szalai Szabolcs <sz.szalai16@gmail.com>
2021-10-31 16:58:20 +0100

    layout kész

--- app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
+++ app/src/main/java/hu/petrik/akasztofa/MainActivity.kt
@@ -2,6 +2,9 @@ package hu.petrik.akasztofa
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
